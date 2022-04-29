088c37900085163be1fdcc951f6a59129f8d22a3
trobutlef <astuhuamant@gmail.com>
2022-04-28 22:11:08 -0500

    header & imgs update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
 import Header from './Header';
 function App() {
   return (
-    //BEM convention -> it is just a name styling
+    //BEM convention -> it is just a name styling for tags in html
     <div className="app">
       <h1> Welcome to Andes </h1>
-      <p> Where everything is better than Amazon </p>
-      {/* HEADER */Header()}
+      <p> Where everything is better than Amazon lol </p>
+      <Header/>
       {/* HOME */}  
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,32 @@
 import React from 'react'
 import './Header.css'
+import template from './img/andes_logos.jpg'
 
 function Header() {
   return (
+
     <div className = 'header'>
-      <img className="header_logo" src="img/andes_logos.jpg"/>
+
+      <img className="header_logo" src={template}/>
+
       <div className="header_search">
-        <input 
-          className = "header_searchInput" type="text"/>
+        <input className = "header_searchInput" type="text"/>
           {/*Logo*/}
       </div>
-     <div className="header_nav"> 
-      
-      <div className="header_option"> {/* Sign in*/}
-        <span className='header_optionLineOne'> Hello Guest </span>
-        <span className='header_optionLineTwo'> Sign In </span>
+
+      <div className="header_nav"> 
+        <div className="header_option"> {/* Sign in*/}
+          <span className='header_optionLineOne'> Hello Guest </span>
+          <span className='header_optionLineTwo'> Sign In </span>
+        </div>
+
+        <div className="header_option"> {/* Return & orders*/}
+        </div>
+
+        <div className="header_option"> {/* Prime*/}
+        </div>
+
       </div>
-
-      <div className="header_option"> {/* Return & orders*/}
-
-      </div>
-
-      <div className="header_option"> {/* Prime*/}
-
-      </div>
-     </div>
-
     </div>
   )
 }
